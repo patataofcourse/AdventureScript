cb2eae759f12a108693ee9919399402ec1f8d379
patataofcourse <patataofcourse@gmail.com>
2021-10-10 14:23:45 +0200

    Get query working

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+mod commands {
+    
+}
+
+pub mod io {
+    use std::io;
+
+    pub fn show(text: &str) {
+        println!("{}", text);
+    }
+
+    pub fn wait() {
+        println!("would wait for input here");
+    }
+
+    pub fn query(text: &str, choices: Vec<&str>, allow_save: bool) -> u8 {
+        if text != "" {
+            show(&text);
+        }
+
+        let mut c = 1;
+        for ch in &choices {
+            show(&format!("{}. {}", c, ch));
+            c+=1;
+        }
+
+        let mut result;
+        loop {
+            //print!(">");
+            result = String::new();
+            io::stdin().read_line(&mut result)
+                .expect("Failed to read line");
+            
+            match result.trim() {
+                "s" => {
+                    if allow_save {
+                        show(&String::from("Would save here"));
+                    }
+                }
+                "r" => {
+                    if allow_save {
+                        show(&String::from("Would restore here"));
+                    }
+                }
+                "q" => {
+                    return 0
+                }
+                _ => (),
+            }
+
+            let num_result : u8 = match result.trim().parse() {
+                Ok(n) => n,
+                Err(_) => continue,
+            };
+
+            if (num_result as usize) < choices.len() {return num_result}
+
+        }
+    }
+
+    //TODO: add load_file function
+}
+
+mod parsing {
+
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use adventure_script::io as as_io;
+
+fn main() {
+    as_io::query("", vec!["one","two","three"], true);
+}
