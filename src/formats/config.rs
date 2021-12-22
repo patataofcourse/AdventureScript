cf08f8723fd1b1df1db12964ad403a7e60043533
patataofcourse <patataofcourse@gmail.com>
2021-12-22 17:05:31 +0100

    SemVer on config

--- src/formats/config.rs
+++ src/formats/config.rs
@@ -2,15 +2,44 @@ use crate::core::{
     error::{ASFileError, FileErrors},
     FileType, GameInfo,
 };
+use semver::Version;
 use serde_derive::Deserialize;
 use std::{io::Read, path::PathBuf};
 
+// Needed, since Version can't be serialized/deserialized
 #[derive(Deserialize, Debug)]
+struct UnparsedVerConfig {
+    pub name: String,
+    pub description: Option<String>,
+    pub version: String,
+    pub icon: Option<PathBuf>,
+    pub module: Option<Vec<Module>>,
+}
+
+impl UnparsedVerConfig {
+    pub fn parse_ver(self) -> anyhow::Result<Config> {
+        Ok(Config {
+            name: self.name,
+            description: self.description,
+            version: match Version::parse(&self.version) {
+                Ok(c) => c,
+                Err(e) => Err(ASFileError::from(
+                    "info.toml",
+                    "r",
+                    FileErrors::ConfigLoadError(format!("Error parsing field 'version': {}", e)),
+                ))?,
+            },
+            icon: self.icon,
+            module: self.module,
+        })
+    }
+}
+
+#[derive(Debug)]
 pub struct Config {
     pub name: String,
     pub description: Option<String>,
-    //TODO: make the version a SemVer
-    pub version: String,
+    pub version: Version,
     pub icon: Option<PathBuf>,
     pub module: Option<Vec<Module>>,
 }
@@ -21,11 +50,11 @@ pub struct Module {
     pub file: Option<PathBuf>,
 }
 
-pub fn load_config(info: &GameInfo) -> anyhow::Result<Config> {
+pub(crate) fn load_config(info: &GameInfo) -> anyhow::Result<Config> {
     let mut file = String::from("");
     info.load_file("info.toml", "r", FileType::Other)?
         .read_to_string(&mut file)?;
-    let config: Config = match toml::from_str(&file) {
+    let config: UnparsedVerConfig = match toml::from_str(&file) {
         Ok(c) => c,
         Err(e) => Err(ASFileError::from(
             "info.toml",
@@ -33,5 +62,5 @@ pub fn load_config(info: &GameInfo) -> anyhow::Result<Config> {
             FileErrors::ConfigLoadError(e.to_string()),
         ))?,
     };
-    Ok(config)
+    config.parse_ver()
 }
