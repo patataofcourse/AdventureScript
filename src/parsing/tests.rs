11fd752e51b27e7a0327f00522db8a829d9ba319
patataofcourse <patataofcourse@gmail.com>
2022-01-12 17:10:10 +0100

    Fix simplify_test

--- src/parsing/tests.rs
+++ src/parsing/tests.rs
@@ -1,7 +1,7 @@
 #[test]
 fn simplify_test() {
     let text = "'hel\"lo' \"hel'lo\" 'hel\\'lo' \"hel\\\"lo\"".to_string();
-    let (s, q) = super::simplify(text, super::SimplifyMode::Strings).unwrap();
+    let (s, q) = super::simplify(text).unwrap();
     assert_eq!(s, "\"0\" \"1\" \"2\" \"3\"");
     assert_eq!(q, vec!["hel\"lo", "hel'lo", "hel\\'lo", "hel\\\"lo"]);
 }
